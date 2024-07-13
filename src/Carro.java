c1b72e18aaf1d3036da41559665d2a15fdaa9faf
Maurício Martins <mauriciocamartins@gmail.com>
2024-07-12 23:01:48 -0300

    Adicionando comportamentos nos objetos atraves de Metodos
    Signed-off-by: Maurício Martins <mauriciocamartins@gmail.com>

--- src/Carro.java
+++ src/Carro.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Carro {
+public class Carro {
+
+    int cavalos;
+    String cor;
+    String transmissao;
+    String montadora;
+    int capacidadeTanque;
+    int combustivel;
+    int kmPorLitros;
+
+    void abastercer(int litros){
+        combustivel = combustivel + litros;
+    }
+
+    int autonomiaCombustivel(){
+        int autonomia = kmPorLitros * combustivel;
+        return autonomia;
+    }
 }
--- src/MainCarro.java
+++ src/MainCarro.java
@@ -1,2 +1,35 @@
-package PACKAGE_NAME;public class MainCarro {
+public class MainCarro {
+    public static void main(String[] args) {
+
+        Carro punto = new Carro();
+        punto.cavalos = 100;
+        punto.cor = "Preta";
+        punto.montadora = "Fiat";
+        punto.transmissao = "Manual";
+        punto.capacidadeTanque = 50;
+        punto.combustivel = 0;
+        punto.kmPorLitros = 10;
+
+        Carro civic = new Carro();
+        civic.cavalos = 150;
+        civic.cor = "Prata";
+        civic.montadora = "Honda";
+        civic.transmissao = "Automatica";
+        civic.capacidadeTanque = 48;
+        civic.combustivel = 10;
+        civic.kmPorLitros = 8;
+
+        System.out.println("Punto: " + punto.combustivel);
+        System.out.println("Civic: " + civic.combustivel);
+
+        punto.abastercer(20);
+        civic.abastercer(30);
+
+        System.out.println("Punto, depois de abastecer: " + punto.combustivel);
+        System.out.println("Civic, depois de abastecer: " + civic.combustivel);
+
+        System.out.println("Autonomia Punto: " + punto.autonomiaCombustivel());
+        System.out.println("Autonomia Civic: " + civic.autonomiaCombustivel());
+
+    }
 }
